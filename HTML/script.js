04427a4c4dd617bf9a3c9f90bac6cb9805660dad
ElisaMarielle <elisinha220419@gmail.com>
2026-03-04 13:40:23 -0300

    atualizacao

--- HTML/script.js
+++ HTML/script.js
@@ -4,3 +4,30 @@ const nav = document.querySelector('.navegar nav');
 menuBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+function salvarLocal(){
+  localStorage.setItem("comentario", JSON.stringify(comentario));
+  window.location.reload();
+   
+}
+
+//modalCli > modalComent
+function abrirModal(){
+  modalComent.style.display = "block";
+  limparCampos();
+}
+
+function fecharModal(){
+    modalComent.style.display = "none";
+}
+
+//formCli > comentario
+const formComent = document.getElementById("comentario");
+formComent.addEventListener("submit", e => {
+    e.preventDefault();
+    limparCampos();
+})
+
+function limparCampos(){
+  document.getElementById("coment").value = " ";
+}
